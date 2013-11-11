266694d2eb5c5e23caf0796b63ae4ad438f5769a
Nitya Oberoi <nitya@yipit.com>
2013-11-11 10:52:05 -0500

    Social icons, inheriting templates

--- main.py
+++ main.py
@@ -11,6 +11,18 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+@app.route('/clients/')
+def clients():
+    return render_template('clients.html')
+
+@app.route('/services/')
+def services():
+    return render_template('services.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
