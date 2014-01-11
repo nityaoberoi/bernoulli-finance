7f2434c036b6fe4a21dc3e659fbaf01b1627d0be
Nitya Oberoi <nitya.oberoi@gmail.com>
2014-01-11 21:13:24 +0530

    Contact page, underlining top nav links, smaller footer, fixing header images

--- main.py
+++ main.py
@@ -23,6 +23,10 @@ def clients():
 def services():
     return render_template('services.html')
 
+@app.route('/contact/')
+def contact():
+    return render_template('contact.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
